4469fd606de50f5b2d5bc7d8aa258daa2554af42
f.areias <nando.calheirosx@gmail.com>
2024-10-25 07:47:38 -0300

    Add q1, q2 and q3

--- to_xlsx2.py
+++ to_xlsx2.py
@@ -1,17 +1,108 @@
 import pandas as pd
+import os
+import time
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
+translation_dict = {
+    "PT": "Publication Type",
+    "AU": "Authors",
+    "BA": "Book Authors",
+    "BE": "Book Editors",
+    "GP": "Book Group Authors",
+    "AF": "Author Full Names",
+    "BF": "Book Author Full Names",
+    "CA": "Group Authors",
+    "TI": "Article Title",
+    "SO": "Source Title",
+    "SE": "Book Series Title",
+    "BS": "Book Series Subtitle",
+    "LA": "Language",
+    "DT": "Document Type",
+    "CT": "Conference Title",
+    "CY": "Conference Date",
+    "CL": "Conference Location",
+    "SP": "Conference Sponsor",
+    "HO": "Conference Host",
+    "DE": "Author Keywords",
+    "ID": "Keywords Plus",
+    "AB": "Abstract",
+    "C1": "Addresses",
+    "C3": "Affiliations",
+    "RP": "Reprint Addresses",
+    "EM": "Email Addresses",
+    "RI": "Researcher Ids",
+    "OI": "ORCIDs",
+    "FU": "Funding Orgs",
+    "FP": "Funding Name Preferred",
+    "FX": "Funding Text",
+    "CR": "Cited References",
+    "NR": "Cited Reference Count",
+    "TC": "Times Cited, WoS Core",
+    "Z9": "Times Cited, All Databases",
+    "U1": "180 Day Usage Count",
+    "U2": "Since 2013 Usage Count",
+    "PU": "Publisher",
+    "PI": "Publisher City",
+    "PA": "Publisher Address",
+    "SN": "ISSN",
+    "EI": "eISSN",
+    "BN": "ISBN",
+    "J9": "Journal Abbreviation",
+    "JI": "Journal ISO Abbreviation",
+    "PD": "Publication Date",
+    "PY": "Publication Year",
+    "VL": "Volume",
+    "IS": "Issue",
+    "PN": "Part Number",
+    "SU": "Supplement",
+    "SI": "Special Issue",
+    "MA": "Meeting Abstract",
+    "BP": "Start Page",
+    "EP": "End Page",
+    "AR": "Article Number",
+    "DI": "DOI",
+    "DL": "DOI Link",
+    "D2": "Book DOI",
+    "EA": "Early Access Date",
+    "PG": "Number of Pages",
+    "WC": "WoS Categories",
+    "WE": "Web of Science Index",
+    "SC": "Research Areas",
+    "GA": "IDS Number",
+    "PM": "Pubmed Id",
+    "OA": "Open Access Designations",
+    "HC": "Highly Cited Status",
+    "HP": "Hot Paper Status",
+    "DA": "Date of Export",
+    "UT": "UT (Unique WOS ID)",
+}
+
 def load_file(file):
     logging.info(f"Lendo arquivo: {file}")
-    data = pd.read_csv(file, sep='\t', header=0, names=["Affiliations", "Record Count", "% of 9.977"])
+    data = pd.read_csv(file, sep='\t', header=0)
+    data.rename(columns=translation_dict, inplace=True)
     return data
 
-file_path = 'datasets/analyze (3).txt'
-data = load_file(file_path)
+input_file = 'datasets/atividade02/Q3.txt'
+if not os.path.exists(input_file):
+    raise FileNotFoundError(f"Arquivo '{input_file}' não encontrado. Verifique o caminho.")
 
-output_file = 'datasets/affiliations_output.xlsx'
-data.to_excel(output_file, index=False)
+start_time = time.time()
+
+data_frame = load_file(input_file)
+
+output_file = 'datasets/atividade02/Q3.xlsx'
+data_frame.to_excel(output_file, index=False)
+
+end_time = time.time()
+
+execution_time = end_time - start_time
+total_migrated_lines = len(data_frame)
+file_size = os.path.getsize(output_file) / (1024 * 1024)
 
 logging.info(f"Dados combinados salvos em {output_file}")
+logging.info(f"Tempo total de execução: {execution_time:.2f} segundos")
+logging.info(f"Total de linhas migradas: {total_migrated_lines}")
+logging.info(f"Tamanho do arquivo final: {file_size:.2f} MB")
